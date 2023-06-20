7ae747e55f6e7cf82f926f468ee84b65b31749e1
danbaissa <danbaissa@gmail.com>
2023-06-20 17:57:40 -0400

    Its working now

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # main.py
 
-from preprocess import preprocess_data
+import matplotlib.pyplot as plt
+from preprocess import preprocess_data, load_raster_data
 from model import create_model
 
 def main():
@@ -13,8 +14,19 @@ def main():
     # Train the model
     model.fit(X_train, y_train, epochs=10, batch_size=64, validation_data=(X_test, y_test))
 
-    # Evaluate the model
-    model.evaluate(X_test, y_test)
+    # Evaluate the model on the test data
+    loss = model.evaluate(X_test, y_test)
+    print(f"Out-of-sample MSE: {loss}")
+
+    # Use the model to make predictions on the full DSMP dataset
+    BM_data = load_raster_data("BM_resampled.tif")
+    BM_data = np.expand_dims(BM_data, axis=(0, 3))  # Add dimensions to fit keras Conv2D input shape
+    DSMP_improved = model.predict(BM_data)[0, :, :, 0]
+
+    # Visualize the predicted DSMP raster
+    plt.imshow(DSMP_improved, cmap='gray')
+    plt.show()
+
 
 if __name__ == "__main__":
     main()
